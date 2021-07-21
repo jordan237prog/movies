4a61b7c498ea3cc555ddf13e1d09dea5bd076add
jordan237prog <lordmoyojordan@gmail.com>
2021-07-21 11:36:28 +0200

    pagination  ajoutée et testée

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactPaginate from 'react-paginate';
 
 import Movie from '../../Components/Movie'
 import {movies$} from '../../data/movies'
@@ -8,26 +9,60 @@ import './style.css'
 function Home() {
 
   const [movies, setMovies] = React.useState(null)
+  const [offset, setOffset] = React.useState(0)
+  const [moviesPerPage, setMoviesPerPage] = React.useState(4)
+  const [currentPageNumber, setCurrentPageNumber] = React.useState(0)
 
    React.useEffect(()=>{ 
      movies$.then(
-       (results) => setMovies(results)
-     )
+       (results) => {
+        setMovies(results)
+      })
   },[]); 
+
+  const pagesVisited = currentPageNumber * moviesPerPage
+
+  const diaplayMovies = movies!==null && movies.slice(pagesVisited, pagesVisited + moviesPerPage)
+    .map(movie => {
+      return (
+        <Movie 
+          key={movie.id}
+          movieData={movie}
+          image="https://source.unsplash.com/user/c_v_r"
+          setMovies={setMovies}
+          movies={movies}
+        />
+      );
+    });
+
+    const pageCount = movies!==null && Math.ceil(movies.length / moviesPerPage);
+
+    const changePage = ({selected}) => {
+      setCurrentPageNumber(selected)
+    }
+
 
   if (movies === null) {
       return  <div></div>;
     }
     return  (
     <div className='container'>
-      <div className='movieContainer'>
-        { 
-           movies.map((movie)=>{ 
-             var image = "https://source.unsplash.com/user/c_v_r"
-            return <Movie key={movie.id} movieData={movie} image={image} setMovies={setMovies} movies={movies} />;
-          })
-        }
+      <div className='movieContainer'> 
+        {diaplayMovies}
       </div>
+      <ReactPaginate
+          previousLabel={"prev"}
+          nextLabel={"next"}
+          breakLabel={"..."}
+          pageCount={pageCount}
+          breakClassName={"break-me"}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={changePage}
+          containerClassName={"pagination"}
+          subContainerClassName={"pages pagination"}
+          activeClassName={"active"}
+        />
     </div>
   ) 
   }
